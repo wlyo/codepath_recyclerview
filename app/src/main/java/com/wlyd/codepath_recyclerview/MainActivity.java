1ab6dc3324b41f0d557faa66e62201c88e82b7bd
unknown <wlydouang+1@gmail.com>
2018-03-27 16:22:47 +0200

    binding Adapter to the RecyclerView

--- app/src/main/java/com/wlyd/codepath_recyclerview/MainActivity.java
+++ app/src/main/java/com/wlyd/codepath_recyclerview/MainActivity.java
@@ -2,12 +2,28 @@ package com.wlyd.codepath_recyclerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Contact> contacts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvContacts = findViewById(R.id.rvContacts);
+
+        contacts = Contact.createContactsList(20);
+
+        ContactsAdapter adapter = new ContactsAdapter(this, contacts);
+
+        rvContacts.setAdapter(adapter);
+
+        rvContacts.setLayoutManager(new LinearLayoutManager(this));
     }
 }
